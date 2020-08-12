Fix normalization

scripts/test_reg.py
@@ -18,7 +18,7 @@ from external.dada.io import print_config
 from external.dada.logger import Logger
 
 from selectivenet.vgg_variant import vgg16_variant
-from selectivenet.model import SelectiveNet
+from selectivenet.model import SelectiveNet, SelectiveNetRegression
 from selectivenet.loss import SelectiveLoss
 from selectivenet.data import DatasetBuilder
 from selectivenet.evaluator import Evaluator
@@ -29,12 +29,12 @@ import wandb
 WANDB_PROJECT_NAME="selective_net"
 if "--unobserve" in sys.argv:
     os.environ["WANDB_MODE"] = "dryrun"
-wandb.init(project=WANDB_PROJECT_NAME, tags=["pytorch", "test"])
+wandb.init(project=WANDB_PROJECT_NAME, tags=["pytorch", "test", "regression"])
 
 # options
 @click.command()
 # model
-@click.option('--dim_features', type=int, default=512)
+@click.option('--dim_features', type=int, default=64)
 @click.option('--dropout_prob', type=float, default=0.3)
 @click.option('-c', '--checkpoint', type=str, default='setarehc/selective_net', help='checkpoint path')
 @click.option('-w', '--weight', type=str, default='final', help='model weight to load') # final, best_val or best_val_tf
@@ -44,7 +44,7 @@ wandb.init(project=WANDB_PROJECT_NAME, tags=["pytorch", "test"])
 @click.option('-d', '--dataset', type=str, required=True)
 @click.option('--dataroot', type=str, default='/home/setarehc/selectivenet_pytorch/data', help='path to dataset root')
 @click.option('-j', '--num_workers', type=int, default=8)
-@click.option('-N', '--batch_size', type=int, default=128)
+@click.option('-N', '--batch_size', type=int, default=256)
 @click.option('--normalize', is_flag=True, default=True)
 @click.option('--augmentation', type=str, default='original', help='type of augmentation set to original, tf or lili') # just for trials
 # loss
@@ -69,8 +69,7 @@ def test(**kwargs):
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=FLAGS.batch_size, shuffle=False, num_workers=FLAGS.num_workers, pin_memory=True)
 
     # model
-    features = vgg16_variant(dataset_builder.input_size, FLAGS.dropout_prob).cuda()
-    model = SelectiveNet(features, FLAGS.dim_features, dataset_builder.num_classes, div_by_ten=FLAGS.div_by_ten).cuda()
+    model = SelectiveNetRegression(dataset_builder.input_size, FLAGS.dim_features).cuda()
     model_config = wandb.restore('flags.json', run_path=os.path.join(FLAGS.checkpoint, FLAGS.exp_id), replace=True)
     print_config(path=model_config.name)
     best_model = wandb.restore(os.path.join('checkpoints', 'checkpoint_{}.pth'.format(FLAGS.weight)), run_path=os.path.join(FLAGS.checkpoint, FLAGS.exp_id), replace=True) # model file
@@ -87,8 +86,8 @@ def test(**kwargs):
         threshold = 0.5
 
     # loss
-    base_loss = torch.nn.CrossEntropyLoss()
-    SelectiveCELoss = SelectiveLoss(base_loss, coverage=FLAGS.coverage)
+    base_loss = torch.nn.MSELoss()
+    SelectiveCELoss = SelectiveLoss(base_loss, coverage=FLAGS.coverage, regression=True)
    
     # pre epoch
     test_metric_dict = MetricDict()
@@ -105,6 +104,7 @@ def test(**kwargs):
 
             # compute selective loss
             loss_dict = OrderedDict()
+            #import pdb; pdb.set_trace()
             loss_dict = SelectiveCELoss(out_class, out_select, out_aux, t, threshold, mode='test')
             loss = loss_dict['loss_pytorch']
             loss_dict['loss_pytorch'] = loss.detach().cpu().item()

selectivenet/model.py
@@ -3,7 +3,7 @@ class SelectiveNetRegression(torch.nn.Module):
     """
     SelectiveNet for regression with rejection option.
     """
-    def __init__(self, input_dim:int, dim_features:int, init_weights=True):
+    def __init__(self, input_dim:int, dim_features:int, init_weights=True, div_by_ten=False):
         """
         Args
             input_dim: dimension of input vector.
@@ -12,6 +12,7 @@ class SelectiveNetRegression(torch.nn.Module):
         super(SelectiveNetRegression, self).__init__()
         self.input_dim = input_dim
         self.dim_features = dim_features
+        self.div_by_ten = div_by_ten
 
         # main body block
         self.feature_extractor = torch.nn.Sequential(
@@ -26,10 +27,13 @@ class SelectiveNetRegression(torch.nn.Module):
         )
 
         # represented as g() in the original paper
-        self.selector = torch.nn.Sequential(
+        self.pre_selector = torch.nn.Sequential(
             torch.nn.Linear(self.dim_features, 16),
             torch.nn.ReLU(True),
-            torch.nn.BatchNorm1d(16), 
+            torch.nn.BatchNorm1d(16) 
+        )
+
+        self.post_selector = torch.nn.Sequential(
             torch.nn.Linear(16, 1),
             torch.nn.Sigmoid()
         )
@@ -43,7 +47,8 @@ class SelectiveNetRegression(torch.nn.Module):
         if init_weights:
             self._initialize_weights(self.feature_extractor)
             self._initialize_weights(self.predictor)
-            self._initialize_weights(self.selector)
+            self._initialize_weights(self.pre_selector)
+            self._initialize_weights(self.post_selector)
             self._initialize_weights(self.aux_predictor)
         
 
@@ -53,7 +58,10 @@ class SelectiveNetRegression(torch.nn.Module):
         
         prediction_out = self.predictor(x)
 
-        selection_out= self.selector(x)
+        selection_out= self.pre_selector(x)
+        if self.div_by_ten:
+            selection_out /= 10.0
+        selection_out = self.post_selector(selection_out)
     
         auxiliary_out  = self.aux_predictor(x)
 